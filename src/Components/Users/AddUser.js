cf68a24fff1e5b4a16b6da9e808e9d9e0ba51ac0
Nitin Patel <patel904@sheridancollege.ca>
2021-09-13 20:03:38 -0400

    Adding reusable Button Component

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from '../Users/AddUser.module.css';
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
 
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+        }
+
+        if(enteredAge < 0){
+            return;
+        }
+
+        console.log(enteredUsername);
+        console.log(enteredAge);    
     }
+
+    const userNameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    };
 
     return(
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
             
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" placeholder="Enter Username"/>
+            <input type="text" id="username" placeholder="Enter Username" onChange={userNameChangeHandler}/>
 
             <label htmlFor="age">Age</label>
-            <input type="text" id="age" placeholder="Enter Age"/>
+            <input type="number" id="age" placeholder="Enter Age" step="1" onChange={ageChangeHandler}/>
  
-             <button type="submit">Add User</button>
+             <Button type="submit">Add User</Button>
             
             </form>
         </Card>
